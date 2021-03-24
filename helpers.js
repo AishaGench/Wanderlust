12a75cd79aa8cd67bb9a8862e15ddd6176a6985e
Aisha Gench <aisha.gench@gmail.com>
2021-03-24 15:03:31 -0500

    Created project

---
+++ helpers.js
@@ -0,0 +1,32 @@
+
+const createWeatherHTML = (currentDay) => {
+  console.log(currentDay)
+  const weatherContent = `
+    <h2>${weekDays[(new Date()).getDay()]}</h2>
+		<h2>Temperature: ${currentDay.current.temperature}&deg;C</h2>
+		<h2>Condition: ${currentDay.current.weather_descriptions[0]}</h2>
+    <img src="${currentDay.current.weather_icons[0]}">`;
+    $weatherDiv.append(weatherContent);
+}
+
+const kelvinToFahrenheit = k => ((k - 273.15) * 9 / 5 + 32).toFixed(0);
+
+
+const createVenuesHTML = (data) => {
+  
+  $destination.append(`<h2>${data[0].venue.location.city}</h2>`);
+  data.forEach((item, index) => {
+    const venueIcon = item.venue.categories[0].icon;
+    const venueImgSrc = `${venueIcon.prefix}bg_64${venueIcon.suffix}`;
+    const venueContent = `
+    <div class="venue" id="venue${index}">
+      <h2>${item.venue.name}</h2>
+      <img class="venueimage" src="${venueImgSrc}"/>
+      <h3>Address:</h3>
+      <p>${item.venue.location.address}</p>
+      <p>${item.venue.location.city}</p>
+      <p>${item.venue.location.country}</p>
+    </div>`;
+    $venueDivs.append(venueContent);
+});
+}
---
+++ script.js
@@ -0,0 +1,28 @@
+// Page Elements
+const inputField = document.getElementById('city');
+const submitBtn = document.getElementById('button');
+
+// Foursquare API Info
+// const clientId = '1FYL3UDSA4FEATQF0GZJJON1TCPWVYCK1QISOZPOYZG1IBG5';
+// const clientSecret = '4HULZXBJV0H1P3RKYBRH5IPFQRP44FKGDWUILLYTML4QXA4A';
+
+// // WEATHER API Info
+
+
+
+// Add AJAX functions here:
+
+const getVenues = async () => {
+  
+}
+
+const getForecast = async () => {
+  
+}
+
+
+
+
+
+
+// Execute function
